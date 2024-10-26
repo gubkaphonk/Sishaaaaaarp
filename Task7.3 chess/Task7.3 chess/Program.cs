1ca346ca167026e7723e886cec3f87fbd6346be0
gubkaphonk <dkhalzanov@yandex.ru>
2024-10-27 01:43:00 +0500

    Выполнено задание 7.1

---
+++ Task7.3 chess/Task7.3 chess/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._3_chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
