630fc988d490048306a5e2133324ff96856154c3
gubkaphonk <dkhalzanov@yandex.ru>
2024-10-27 23:18:08 +0500

    Выполненно задание 7.2

---
+++ Finally 7.2/Finally 7.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение y");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Результат выражения: " + logic(x, y));
+            Console.ReadKey();
+        }
+        static bool logic(int x, int y)
+        {
+            if ((y >= 1 || y <= -3) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
